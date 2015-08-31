a795774839d2328a297096b4731767aa76c5c321
buty <631489527@qq.com>
2015-08-31 15:01:47 +0800

    城市列表js

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+// $a = require 'dis_common_district.json';
+
+// $a = json_decode($a, true);
+
+// $res = [];
+// foreach($a as $k => $v) {
+//     if($v['level'] < 3) {
+//         $res[$k] = array_values($v);
+//     }
+// }
+
+// $json_string = json_encode($res, JSON_UNESCAPED_UNICODE);
+
+// file_put_contents('./dis_common_district.json', $json_string);
